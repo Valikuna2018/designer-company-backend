7f55de3824d57c3bb8f2386c7a55e464eefa4535
Valeriane <valerianemur@gmail.com>
2026-02-25 21:40:13 +0400

    connection to database and project structure

---
+++ designer-company-backend/src/main/java/com/example/designercompanybackend/DesignerCompanyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.designercompanybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignerCompanyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignerCompanyBackendApplication.class, args);
+    }
+
+}
---
+++ designer-company-backend/src/test/java/com/example/designercompanybackend/DesignerCompanyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.designercompanybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignerCompanyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
